bb77791634f29a59a0a3a63553856b1da413c1c2
Cho <37912868+vh5202002@users.noreply.github.com>
2022-12-06 11:09:01 -0800

    final v

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./About.css";
+import aboutImg from "../../images/about-img.jpg";
+
+const About = () => {
+    return (
+        <section className="about">
+            <div className="container">
+                <div className="section-title">
+                    <h2>About</h2>
+                </div>
+
+                <div className="about-content grid">
+                    <div className="about-img">
+                        <img src = {aboutImg} alt = "" />
+                    </div>
+                    <div className="about-text">
+                        <h2 className="about-title fs-26 ls-1">About BookHub</h2>
+                        <p className="fs-17">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt aliquid, exercitationem ea fugit voluptates blanditiis repellat quo error, in necessitatibus earum totam aspernatur ipsa quasi! Mollitia delectus error sunt dolore!</p>
+                        <p className="fs-17">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, dicta, possimus inventore eveniet atque voluptatibus repellendus aspernatur illo aliquam dignissimos illum. Commodi, porro omnis dolore amet neque modi quas eum!</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default About
